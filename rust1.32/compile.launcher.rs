0f720fbb8dee52ff5abb78476c77e7b39fe17651
Roberto Diaz <roberto@theagilemonkeys.com>
2019-03-04 00:22:52 +0000

    optimization in error path

--- rust1.32/compile.launcher.rs
+++ rust1.32/compile.launcher.rs
@@ -9,9 +9,9 @@ use std::os::unix::io::FromRawFd;
 use std::collections::HashMap;
 use serde_json::{Value, Error};
 use actions::main as actionMain;
-use std::mem::drop;
 
 fn main() {
+    let mut fd3 = unsafe { File::from_raw_fd(3)};
     loop {
         let mut buffer = String::new();
         io::stdin().read_line(&mut buffer).unwrap();
@@ -24,22 +24,28 @@ fn main() {
                         let mut unparsed_payload:Result<HashMap<String,Value>,Error> = serde_json::from_value(val);
                         match unparsed_payload {
                             Ok(value) => payload = value,
-                            Err(err) => eprintln!("Error parsing value json: {}", err)
+                            Err(err) => {
+                                eprintln!("Error parsing value json: {}", err);
+                                continue
+                            }
                         }
                     } else {
                         env::set_var(format!("__OW_{}", key.to_uppercase()), val.to_string());
                     }
                 }
             }
-            Err(e) => eprintln!("Error: {}", e)
+            Err(e) =>{
+                eprintln!("Error: {}", e);
+                continue
+            }
         }
-        let mut fd3 = unsafe { File::from_raw_fd(3) };
+        
         match serde_json::to_string(&actionMain(payload)){
             Ok(result) => { write!(&mut fd3, "{}\n", result).expect("Error writting on fd3");}
             Err(err) => {  eprintln!("Error parsing resul value json: {}", err);}
         }
-        drop(fd3);
         stdout().flush().expect("Error flushing stdout");
         stderr().flush().expect("Error flushing stderr");
     }
 }
+
