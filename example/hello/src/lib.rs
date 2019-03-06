eaa26f0d7b08c8b1fd38a48da51bd90fce93a867
Michele Sciabarra <msciabarra@apache.org>
2019-03-06 09:18:38 +0100

    hello demo working

--- example/hello/src/lib.rs
+++ example/hello/src/lib.rs
@@ -3,7 +3,14 @@ extern crate serde_json;
 use std::collections::HashMap;
 use serde_json::Value;
 
-pub fn main(mut input_data: HashMap<String, Value>) -> HashMap<String, Value> {
-    input_data.insert("hello".to_string(),Value::String("world".to_string()));
-    input_data
+pub fn main(args: HashMap<String, Value>) -> HashMap<String, Value> {
+    let name_opt = args.get("name");
+    let name = if name_opt.is_some() {
+        name_opt.unwrap().as_str().unwrap()    
+    } else {
+        "stranger"
+    };
+    let mut out = HashMap::new();
+    out.insert("greeting".to_string(), Value::String(format!("Hello, {}", name)));
+    out
 }
