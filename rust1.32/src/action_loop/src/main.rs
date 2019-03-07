9d79c4b413b50d6b899e34b9b3ad03dd45560973
Bruce Adams <ba@us.ibm.com>
2019-03-07 08:23:03 -0500

    Use more of serde's superpowers

--- rust1.32/src/action_loop/src/main.rs
+++ rust1.32/src/action_loop/src/main.rs
@@ -1,52 +1,48 @@
 extern crate actions;
 extern crate libc;
+extern crate serde;
+extern crate serde_derive;
 extern crate serde_json;
 
 use actions::main as actionMain;
+use serde_derive::Deserialize;
+
 use serde_json::{Error, Value};
 use std::collections::HashMap;
 use std::env;
 use std::fs::File;
-use std::io::{self, stderr, stdout, Write};
+use std::io::{stderr, stdin, stdout, Write};
 use std::os::unix::io::FromRawFd;
+
+#[derive(Debug, Clone, PartialEq, Deserialize)]
+struct Input {
+    value: HashMap<String, Value>,
+    #[serde(flatten)]
+    environment: HashMap<String, Value>,
+}
 
 fn main() {
     let mut fd3 = unsafe { File::from_raw_fd(3) };
     loop {
         let mut buffer = String::new();
-        io::stdin().read_line(&mut buffer).unwrap();
-        let parsed_input: Result<HashMap<String, Value>, Error> = serde_json::from_str(&buffer);
-        let mut payload: HashMap<String, Value> = HashMap::new();
+        stdin().read_line(&mut buffer).unwrap();
+        let parsed_input: Result<Input, Error> = serde_json::from_str(&buffer);
         match parsed_input {
-            Ok(n) => {
-                for (key, val) in n {
-                    if key == "value" {
-                        let mut unparsed_payload: Result<HashMap<String, Value>, Error> =
-                            serde_json::from_value(val);
-                        match unparsed_payload {
-                            Ok(value) => payload = value,
-                            Err(err) => {
-                                eprintln!("Error parsing value json: {}", err);
-                                continue;
-                            }
-                        }
-                    } else {
-                        env::set_var(format!("__OW_{}", key.to_uppercase()), val.to_string());
+            Ok(input) => {
+                for (key, val) in input.environment {
+                    env::set_var(format!("__OW_{}", key.to_uppercase()), val.to_string());
+                }
+                match serde_json::to_string(&actionMain(input.value)) {
+                    Ok(result) => {
+                        writeln!(&mut fd3, "{}", result).expect("Error writing on fd3");
+                    }
+                    Err(err) => {
+                        eprintln!("Error formatting result value json: {}", err);
                     }
                 }
             }
-            Err(e) => {
-                eprintln!("Error: {}", e);
-                continue;
-            }
-        }
-
-        match serde_json::to_string(&actionMain(payload)) {
-            Ok(result) => {
-                writeln!(&mut fd3, "{}", result).expect("Error writing on fd3");
-            }
             Err(err) => {
-                eprintln!("Error formatting result value json: {}", err);
+                eprintln!("Error parsing input: {}", err);
             }
         }
         stdout().flush().expect("Error flushing stdout");
